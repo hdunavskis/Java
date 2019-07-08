c9ab8dd2b0b14bfa7370e9e1537e6b218737b51f
Helmuts <hdunavskis@gmail.com>
2019-07-08 19:59:36 +0100

    lab4

---
+++ Practice/src/Driver.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		
+		double u =  3.14;
+		BigDecimal p = new BigDecimal(Double.toString(u));
+		
+		System.out.println(u);
+
+	}
+
+}
